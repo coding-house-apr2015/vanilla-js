98f5fb9877b996c5794c2213464e7c198b7f6899
Chyld Medford <chyld.medford@gmail.com>
2015-04-13 16:37:48 -0700

    fin

--- objects.js
+++ objects.js
@@ -18,5 +18,24 @@ function Cat(name, age){
   this.age = age;
 }
 
-var fluffy = new Cat('miss sassypants', 3);
-console.log('fluffy', fluffy);
+Cat.prototype.purr = function(){
+  console.log(this.name + ' is purring');
+}
+
+var fluffy = new Cat('fluffy', 3);
+fluffy.purr();
+
+var molly = new Cat('molly', 5);
+molly.purr();
+
+var cats = [];
+for(var i = 1; i < 11; i++){
+  var cat = new Cat('cat'+i, i);
+  cats.push(cat);
+}
+
+cats.forEach(function(c){
+  if(c.age > 5){
+    c.purr();
+  }
+});
